823ab983a6d0b5e68d27949ecace24587cb0fd46
wesley601 <wesleypassos601@gmail.com>
2022-01-16 13:45:41 -0300

    feat: add not found filter

---
+++ src/Filters/not-fond.filter.ts
@@ -0,0 +1,15 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Response } from 'express';
+import { EntityNotFoundError } from 'typeorm';
+
+@Catch(EntityNotFoundError)
+export class NotFoundExceptionFilter implements ExceptionFilter {
+  catch(exception: EntityNotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response.status(404).json({
+      message: exception.message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NotFoundExceptionFilter } from './Filters/not-fond.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
+  app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();

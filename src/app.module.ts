2308fc9d8f74ab7160274b4e129c54313aeabfa7
wesley601 <wesleypassos601@gmail.com>
2022-01-16 12:58:59 -0300

    chore: setup movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { Movie } from './movies/entities/movie.entity';
+import { MoviesModule } from './movies/movies.module';
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({ entities: [Movie] }),
+    MoviesModule,
+  ],
   controllers: [],
   providers: [],
 })
